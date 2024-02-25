a48013a7a2a8b54ce6149df0870c655a2ad6987f
harzo <harzojunior@gmail.com>
2024-02-25 12:20:45 +0100

    Revert "chore: pr base"
    This reverts commit bdaa13d0cf01e7bb1bad78ed94b79a3e12faae5a.

---
+++ contract_ilockmvp_sol/contracts/token/ERC20/ILOCKpool.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+//
+// Interlock ERC-20 ILOCK Token Mint Platform
+//
+// Contributors:
+// blairmunroakusa
+// ...
+
+pragma solidity ^0.8.18;
+
+contract ILOCKpool {
+
+}
